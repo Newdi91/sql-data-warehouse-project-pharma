dc200264b91eef1c92a2a31db8f6255de9122e32
Newdi91 <marco.gnudi@libero.it>
2025-09-02 12:35:48 +0200

    Create DDLSilver.sql

---
+++ scripts/gold/DDLSilver.sql
@@ -0,0 +1,30 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema)
+
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
+
+Usage:
+    - These views can be queried directly for analytics and reporting.
+===============================================================================
+*/
+
+-- =============================================================================
+-- Create Dimension: gold_dim_drug
+-- =============================================================================
+
+IF OBJECT_ID('gold_dim_drug', 'V') IS NOT NULL
+    DROP VIEW gold_dim_drug;
+GO
+
+CREATE VIEW gold_dim_drug AS
+SELECT
+	ROW_NUMBER() OVER(ORDER BY drug_name) AS drug_key,
+	drug_name
+FROM silver_drug
+GROUP BY drug_name;
